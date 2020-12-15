476d5fec39533230555610f96a3eee01b8945953
nickarshadi <1477@holbertonschool.com>
2020-12-14 20:36:16 -0500

    Trying to integrate new storage type.

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,21 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiate a new model"""
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            self.id = Column(String(60), nullable=False, primary_key=True)
+            self.created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             if 'updated_at' in kwargs:
                 kwargs['updated_at'] = datetime.\
@@ -38,8 +41,8 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -51,3 +54,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if models.storage_t == "db":
+        cities = relationship("City", backref="state")
+
+    if models.storage_t != 'db':
+        @property
+        def cities(self):
+            """Return list of city instances related to this state."""
+            city_list = []
+            all_cities = models.storage.all(City)
