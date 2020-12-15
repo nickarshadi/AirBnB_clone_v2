b8a047b4c82bd5f4f453bb3aa59ba206c237f136
nickarshadi <1477@holbertonschool.com>
2020-12-15 14:46:20 -0500

    Trying to debug.

--- models/state.py
+++ models/state.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.city import City
-import models
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    """Representation of state """
     if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    if models.storage_t != 'db':
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """Return list of city instances related to this state."""
+            """getter for list of city instances related to the state"""
             city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
