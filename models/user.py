c6fc5484d7f21d27baad41c7b51727b2d0fe0355
jfbm74 <jfbm74@gmail.com>
2020-12-15 21:14:09 -0500

    Fixing pep8 style

--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship, backref
-from models.base_model import BaseModel, Base
-from models.base_model import BaseModel
 
 
 class User(BaseModel, Base):
@@ -13,5 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    reviews = relationship("Review", backref="user", cascade="all, delete")
     places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
