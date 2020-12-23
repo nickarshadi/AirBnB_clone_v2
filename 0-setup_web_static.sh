b1c4e67c8f7b79828bf196ba39119b53094a99e6
nickarshadi <1477@holbertonschool.com>
2020-12-23 14:00:11 -0500

    sed -i ''.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Prepare your web servers.
-apt-get -y update
-apt-get -y upgrade
+# Prepare your web server.
+apt-get -y update && apt-get -y upgrade
 apt-get -y install nginx
-sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default
-sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
-sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Hello World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/ 
-sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+line = "location /hbnb_static/{
+	alias /data/web_static/current/;
+	autoindex off;
+}"
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" etc/nginx/sites-available/default
 service nginx restart
 exit 0
