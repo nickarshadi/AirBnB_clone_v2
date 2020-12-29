9b99d163539cf6013616cf4f34dffc2294a66717
nickarshadi <1477@holbertonschool.com>
2020-12-29 18:56:00 -0500

    Prepare web server.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# Prepare your web server.
+# Prepare your web servers.
 apt-get -y update
 apt-get -y install nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "Hello World!" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
