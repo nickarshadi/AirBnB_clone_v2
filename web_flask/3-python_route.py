5f57192643b14ac51ceae16a4c41d085804bdb16
nickarshadi <1477@holbertonschool.com>
2021-01-19 21:08:17 -0500

    Create a flask web application.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Start a Flask web application."""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Return Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return HBNB."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Display 'C' followed by the text variable."""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """Display python followed by text."""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
